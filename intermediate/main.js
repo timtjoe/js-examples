d124cfb91c45486bc6cc8b497fa9d1eebf5dfe82
timtjoe <ttjoe0@gmail.com>
2026-07-17 08:56:42 +0000

    add promise example

--- intermediate/main.js
+++ intermediate/main.js
@@ -1,11 +1,6 @@
-const counter = (function () {
-    let count = 0;
-    return {
-        increment: () => ++count,
-        value: () => count,
-    }
-})();
-const n = Number(require('fs').readFileSync(0, 'utf-8').trim());
-for (let i = 0; i < n; i++) counter.increment();
-console.log(counter.value());
 
+const promise = new Promise((resolve, reject) => {
+    resolve('hello')
+} )
+
+promise.then(value => console.log(value))
