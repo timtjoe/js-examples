5d3a00e70900809df57bffee120b79647d13ca1c
timtjoe <ttjoe0@gmail.com>
2026-07-17 10:49:12 +0000

    promise pattern done

--- intermediate/main.js
+++ intermediate/main.js
@@ -1,12 +1,10 @@
-function readInt(line) {
-    return Promise.resolve(Number(line))
-}
-
 async function main() {
     const lines = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
-    const a = await readInt(lines[0]);
-    const b = await readInt(lines[1]);
-    console.log(a+b);
+    const promises = lines.map(line => Promise.resolve(Number(line)));
+    // Use Promise.all and sum.
+    const nums = await Promise.all(promises);
+    const sum = nums.reduce((acc, cur) => acc + cur, 0);
+    console.log(sum)
 }
 
 main();
